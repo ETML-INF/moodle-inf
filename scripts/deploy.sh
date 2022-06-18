08fb00250012822d27464cf8a57f0ec917ae3fba
Jonathan Melly <jonathan.melly@gmail.com>
2022-06-18 10:33:22 +0200

    chore(deploy): fix deploy
    + offline on deploy
    + hot reload script update
    + log to file moodle upgrade stuff

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-DB_PROD_NAME="$(grep dbname config.php | awk -F"'" '{print $2}')"
-DB_PROD_HOST="$(grep dbhost config.php | awk -F"'" '{print $2}')"
-DB_PROD_USER="$(grep dbuser config.php | awk -F"'" '{print $2}')"
-DB_PROD_PASSWORD="$(grep dbpass config.php | awk -F"'" '{print $2}')"
+CONFIG="config.php"
+if [ ! -f $CONFIG ]; then
+  echo "Missing config file $CONFIG"
+  exit 1
+fi
+
+DB_PROD_NAME="$(grep dbname $CONFIG | awk -F"'" '{print $2}')"
+DB_PROD_HOST="$(grep dbhost $CONFIG | tail -1 | awk -F"'" '{print $2}')"
+DB_PROD_USER="$(grep dbuser $CONFIG | awk -F"'" '{print $2}')"
+DB_PROD_PASSWORD="$(grep dbpass $CONFIG | awk -F"'" '{print $2}')"
 DB_BACKUP_DIRECTORY="pre-deploy-backups"
 
 REPO="https://github.com/ETML-INF/moodle-inf.git"
 
 # Init repo if needed (FIRST TIME ONLY)
-# this may be done manually...
+# this may need be done manually...
 if [ ! -d .git ]; then
   git init .
   git remote add origin ${REPO}
@@ -21,10 +27,25 @@ if [ ! -d $DB_BACKUP_DIRECTORY ]; then
   mkdir ${DB_BACKUP_DIRECTORY}
 fi
 
-#Upgrade...
-echo -e "Moodle OFFLINE\n" &&  php admin/cli/maintenance.php --disable && \
-echo -e "Backup DB\n" && mysqldump --add-drop-table -h $DB_PROD_HOST -u $DB_PROD_USER --password=$DB_PROD_PASSWORD $DB_PROD_NAME | gzip -v > ${DB_BACKUP_DIRECTORY}/moodle-pre-deploy-$(date +%Y%m%d%-H%M%S).sql.gz && \
-echo -e "Git PULL\n" &&  git pull && \
-echo -e "Git Submodule update\n" &&  bash ./scripts/git-sub-update-init.sh && \
-echo -e "Moodle upgrade\n" &&  php admin/cli/upgrade.php --non-interactive && \
-echo -e "Moodle ONLINE\n" &&  php admin/cli/maintenance.php --disable
+#OFFLINE
+echo -e "Moodle OFFLINE\n" &&  php admin/cli/maintenance.php --enable
+LAST=$?
+if [ $LAST -eq 0 ] ; then
+  #Reload script if needed
+  git fetch && git diff --stat origin/main | grep "$0"
+  LAST=$?
+  if [ $LAST -eq 0 ] ; then
+    echo "Pull and restart updated script $0"
+    git pull && bash "$0" && exit 0
+  else
+    echo -e "Backup DB\n" && mysqldump --add-drop-table -h "$DB_PROD_HOST" -u "$DB_PROD_USER" --password="$DB_PROD_PASSWORD" "$DB_PROD_NAME" | gzip -v > ${DB_BACKUP_DIRECTORY}/moodle-pre-deploy-$(date +%d.%m.%Y-%Hh%Mm%Ss).sql.gz && \
+    echo -e "Git PULL\n" &&  git pull && \
+    echo -e "Git Submodule update\n" &&  bash ./scripts/git-sub-update-init.sh && \
+    echo -e "Moodle upgrade\n" &&  php admin/cli/upgrade.php --non-interactive --verbose-settings > moodle-upgrade-$(date +%d.%m.%Y-%Hh%Mm%Ss).log 2>&1 && \
+    echo -e "Moodle ONLINE\n" &&  php admin/cli/maintenance.php --disable
+  fi
+else
+  echo "Cannot go offline, stopping deploy"
+fi
+
+
