30ce39bb8e7973ac5ecf63bf2b430ee0e1c660d3
Jonathan Melly <jonathan.melly@gmail.com>
2022-06-18 14:58:48 +0200

    chore(deploy): only ask once + do not ask if --no-interaction + always show countdown

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,13 +21,14 @@ if [ ! -d $DB_BACKUP_DIRECTORY ]; then
 fi
 ################ END CONFIG ##########
 
-#Only
+#Only MODIFY THIS function to avoid script issues
 function deploy()
 {
-  #DELAY to stop
+  SHORT_STAT=$(git diff --shortstat)
+  #DELAY to let a last possibility to stop
   for ((i=5;i>=1;i--));
   do
-     echo "Starting deploy in $i secs"
+     echo "Starting deploy [$(SHORT_STAT)] in $i secs (press CTRL-C to exit)"
      sleep 1
   done
 
@@ -40,43 +41,43 @@ function deploy()
   echo -e "Moodle ONLINE\n" &&  php admin/cli/maintenance.php --disable
 }
 
-if [ "$1" = "--force" ] ; then
-  deploy
-else
-  #pre-flight check
-  git fetch --dry-run 2>&1 | grep main
-  LAST=$?
-  if [ $LAST -eq 0 ]; then
-    read -r -p "Do you really want to deploy ? [y/N] " response
-    case "$response" in
-        [yY][eE][sS]|[yY])
-              #Check if script has been modified (reload if needed)
-              git fetch && git diff --stat origin/main | grep "$0"
-              LAST=$?
-              if [ $LAST -eq 0 ] ; then
-                #GO OFFLINE because we will do a pull which may contain something else than deploy script update !!!
-                echo -e "Moodle OFFLINE\n" &&  php admin/cli/maintenance.php --enable
-                LAST=$?
-                if [ $LAST -eq 0 ] ; then
-                  echo "/!\DEPLOY SCRIPT UPDATE DETECTED/!\ : Pull and restart updated script $0"
-                  git pull && bash "$0" --force && exit 0
-                else
-                  echo "Cannot go offline, stopping deploy"
-                fi
-              else
-                #No changes in deploy script, we can continue with that script
-                deploy
-              fi
+function confirmDeploy()
+{
+  echo "#####################################"
+  echo "# READY TO DEPLOY following CHANGES #"
+  echo "#####################################"
+  git diff --compact-summary
+  echo ""
 
-            ;;
-        *)
-            echo "Operation cancelled by user"
-            ;;
-    esac
+  read -r -p "Do you really want to DEPLOY this ? [y/N] " response
+  case "$response" in
+      [yY][eE][sS]|[yY])
+          deploy
+          ;;
+      *)
+          echo "Operation cancelled by user"
+          ;;
+  esac
+
+}
+
+#Check if script has been modified (reload if needed)
+git fetch && git diff --stat | grep "$0"
+LAST=$?
+if [ $LAST -eq 0 ] ; then
+  echo "/!\DEPLOY SCRIPT UPDATE DETECTED - UPDATING/!\ "
+  #GO OFFLINE because we will do a pull which may contain something else than only deploy script update !!!
+  echo -e "Moodle OFFLINE\n" &&  php admin/cli/maintenance.php --enable && \
+    git pull && bash "$0" "$1" && exit 0
+else
+  #No changes in deploy script, we can continue with that script
+  if [ "$1" = "--no-interaction" ] ; then
+    deploy
   else
-    echo "Nothing new in repo, skipping deploy (use --force if needed)"
+    confirmDeploy
   fi
 fi
+
 
 
 
